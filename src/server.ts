8861332ef820fe074ffceac681301b09a3af8c0c
Maxuel Rodrigues <maxuel.rodrigues@gmail.com>
2021-06-26 18:48:07 -0300

    NLW Together Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
+import "reflect-metadata";
 import express from 'express'
+import { router } from "./routes";
+
+import "../db"
 
 const app = express();
 
-app.get('/test', (request, response) => {
-    // request => Entrando
-    // response => Saindo
+app.use(express.json())
+app.use(router);
+/**
+ * !ANOTE ISSO NO CADERNO DEPOIS
+ * TIPOS DE PARAMETROS
+ * Route Params => Parametros que fazem parte da rota
+ * Ex.: localhost:3000/produtos/123118883 ou localhost:3000/produtos/{id}
+ * 
+ * Query Params => Normalmente usado para filtros, são parametros também passados por url
+ * Eles são parametros não obrigatórios e podem não pertencer a url
+ * Ex.: localhost:3000/produtos?name=teclado&description=teclado+bom
+ * 
+ * Body Params => Parametros que vem no corpo da requisição, são parametros normalmente enviados via form ou json
+ * !ANOTE ISSO NO CADERNO DEPOIS
+ */
 
-    return response.send('Olá Mundo (NLW)')
-}) 
-
-app.post('/test-post', (request, response) => {
-    return response.send('Olá método post');
-})
-
-app.listen(3000, () => console.log("Server is running"))
+app.listen(3002, () => console.log("Server is running"))
 
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequest {
+    name: string, 
+    email: string,
+    admin: boolean
+}
+
+class CreateUserService {
+
+    async execute({name, email, admin} : IUserRequest) {
+        const usersRepository = getCustomRepository(UsersRepositories)
+        // VERIFICA SE O EMAIL FOI PASSADO. SE EM BRANCO RETORNA ERRO
+        if(!email) {
+            throw new Error("email incorrect")
+        }
+        // PESQUISA NO BANCO PELO EMAIL
+        const userAlreadyExists = await usersRepository.findOne({email})
+
+        // SE O EMAIL JÁ EXISTE RETORNA ERRO
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        // FAZ A INSTANCIA DO OBJETO USER
+        const user = usersRepository.create({name, email, admin})
+
+        // SALVA O OBJETO EM BANCO
+        await usersRepository.save(user)
+
+        return user;
+    }
+
+}
+
+export { CreateUserService }
