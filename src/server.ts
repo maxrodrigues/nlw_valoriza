c6c7e31eed8ab87f52e9285ca7bd1ef0a4f0cc4f
Maxuel Rodrigues <maxuel.rodrigues@gmail.com>
2021-06-26 20:10:08 -0300

    NLW Together aula 03

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import "express-async-errors"
 import { router } from "./routes";
 
 import "../db"
@@ -8,6 +9,25 @@ const app = express();
 
 app.use(express.json())
 app.use(router);
+
+// ESSE É UM MIDDLEWARE DE TRATAMENTO DE ERRO
+// USANDO AS ROTAS
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    // SE O ERRO FOR DO TPIO THROW ELE VAI RESPONDER
+    // O STATUS 400 COM A MENSAGEM QUE FOI DESCRITA
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    // SE FOR ERRO INTERNO DO SERVIDOR ELE RESPONDE 
+    // ERRO 500 COM A MENSAGEM ABAIXO
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+});
 /**
  * !ANOTE ISSO NO CADERNO DEPOIS
  * TIPOS DE PARAMETROS
